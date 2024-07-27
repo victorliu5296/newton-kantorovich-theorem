f50b1f6b7b7f9e522c66f0fcf4f27fbfac88ebc7
- <victorliu5296@outlook.com>
2024-07-27 15:13:38 -0400

    Update file to follow Mathlib4 conventions

--- NewtonKantorovichTheorem/MeanValueBanach.lean
+++ NewtonKantorovichTheorem/MeanValueBanach.lean
@@ -8,13 +8,18 @@ open Set Function Topology Metric ContinuousLinearMap Filter Units MeasureTheory
 
 section MeanValueBanach
 
-lemma mean_value_theorem_banach_space
-  {X Y : Type*} [NormedAddCommGroup X] [NormedSpace ℝ X]
-  [NormedAddCommGroup Y] [NormedSpace ℝ Y] [CompleteSpace Y]
-  {f : X → Y} {f' : X → X →L[ℝ] Y} {a b : X}
-  (hcont : ContinuousOn (fun t : ℝ ↦ (f' (a + t • (b - a)) : X →L[ℝ] Y)) (Set.Icc 0 1))
-  (hderiv : ∀ t ∈ Set.Icc (0 : ℝ) 1, HasFDerivAt f (f' (a + t • (b - a)) : X →L[ℝ] Y) (a + t • (b - a))) :
-  f b - f a = ∫ (t : ℝ) in (0:ℝ)..(1:ℝ), (f' (a + t • (b - a)) : X →L[ℝ] Y) (b - a) := by
+/-- **Fundamental theorem of calculus-2** or **Mean Value Theorem** for Banach spaces: If `f : X → Y` is differentiable along the line segment
+from `a` to `b`, then the change in `f` equals the integral of its derivative along this path. -/
+lemma integral_eq_sub_of_hasFDerivAt
+    {X Y : Type*} [NormedAddCommGroup X] [NormedSpace ℝ X]
+    [NormedAddCommGroup Y] [NormedSpace ℝ Y] [CompleteSpace Y]
+    {f : X → Y} {f' : X → X →L[ℝ] Y} {a b : X}
+    (hcont : ContinuousOn (fun t : ℝ ↦
+      (f' (a + t • (b - a)) : X →L[ℝ] Y)) (Set.Icc 0 1))
+    (hderiv : ∀ t ∈ Set.Icc (0 : ℝ) 1,
+      HasFDerivAt f (f' (a + t • (b - a)) : X →L[ℝ] Y) (a + t • (b - a))) :
+    f b - f a = ∫ (t : ℝ) in (0:ℝ)..(1:ℝ),
+      (f' (a + t • (b - a)) : X →L[ℝ] Y) (b - a) := by
   -- Step 1: Define the path from a to b
   let γ : ℝ → X := fun t ↦ a + t • (b - a)
   have γ_continuous (t : ℝ) : ContinuousWithinAt γ (Icc 0 1) t := by
@@ -24,27 +29,28 @@ lemma mean_value_theorem_banach_space
     · apply ContinuousWithinAt.smul
       · exact continuousWithinAt_id
       · exact continuousWithinAt_const
-  have hint : IntervalIntegrable (fun t ↦ (f' (γ t)) (b - a)) MeasureTheory.volume 0 1 := by
+  have hint : IntervalIntegrable (fun t ↦ (f' (γ t)) (b - a))
+      MeasureTheory.volume 0 1 := by
     apply ContinuousOn.intervalIntegrable
     simp only [γ]
-    refine ContinuousOn.clm_apply ?hu.hf ?hu.hg
+    apply ContinuousOn.clm_apply
     · simp
       exact hcont
     · exact continuousOn_const
-  have hderiv' : ∀ t ∈ Set.uIcc (0 : ℝ) 1, HasDerivAt (f ∘ γ) ((f' (γ t)) (b - a)) t := by
+  have hderiv' : ∀ t ∈ Set.uIcc (0 : ℝ) 1,
+      HasDerivAt (f ∘ γ) ((f' (γ t)) (b - a)) t := by
     intro t ht
-    refine HasFDerivAt.comp_hasDerivAt t ?hl ?hf
-    · simp [γ]
-      apply hderiv
-      simp at ht
-      simp
-      exact ht
-    · simpa [γ, neg_add_eq_sub] using ((hasDerivAt_const _ a).add ((hasDerivAt_id' t).smul_const (b - a)))
-  have : ∫ (t : ℝ) in (0 : ℝ)..1, (f' (a + t • (b - a))) (b - a) = (f ∘ γ) 1 - (f ∘ γ) 0 := by
+    apply HasFDerivAt.comp_hasDerivAt
+    · aesop
+    · simpa [γ, neg_add_eq_sub] using ((hasDerivAt_const _ a).add
+        ((hasDerivAt_id' t).smul_const (b - a)))
+  have : ∫ (t : ℝ) in (0 : ℝ)..1, (f' (a + t • (b - a))) (b - a)
+      = (f ∘ γ) 1 - (f ∘ γ) 0 := by
     apply intervalIntegral.integral_eq_sub_of_hasDeriv_right_of_le
     · exact zero_le_one' ℝ
     · intro x hx
-      exact ContinuousAt.comp_continuousWithinAt (HasFDerivAt.continuousAt (hderiv x hx)) (γ_continuous x)
+      exact ContinuousAt.comp_continuousWithinAt
+        (HasFDerivAt.continuousAt (hderiv x hx)) (γ_continuous x)
     · intro x hx
       dsimp [γ] at hderiv'
       dsimp [γ]
@@ -56,17 +62,8 @@ lemma mean_value_theorem_banach_space
         · linarith [hx.right]
       exact HasDerivAt.hasDerivWithinAt (hderiv' x hx')
     · exact hint
-
-  convert (intervalIntegral.integral_eq_sub_of_hasDerivAt hderiv' hint) using 1
-  dsimp [γ]
-  have : ∫ (t : ℝ) in (0 : ℝ)..1, (f' (a + t • (b - a))) (b - a) = f b - f a := by
-    have fa : (f ∘ γ) 0 = f a := by
-      dsimp [γ]
-      simp
-    have fb : (f ∘ γ) 1 = f b := by
-      dsimp [γ]
-      simp
-    rw [fb, fa] at this
-    exact this
+  have : ∫ (t : ℝ) in (0 : ℝ)..1, (f' (a + t • (b - a))) (b - a)
+    = f b - f a := by aesop
   exact _root_.id (Eq.symm this)
+
 end MeanValueBanach
